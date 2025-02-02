f8c1f9e1039470b8b3413d32470226cbd3d4d5b2
Darkandr1231 <markandrey05@gmail.com>
2025-02-02 10:43:41 +0600

    seventh

--- actions/login.ts
+++ actions/login.ts
@@ -23,6 +23,15 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
             redirectTo: DEFAULT_LOGIN_REDIRECT,
         })
     } catch (error) {
-        if (error instanceof AuthError) {}
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" }
+                default:
+                    return { error: "Something went wrong!" }
+            }
+        }
+
+        throw error;
     }
 };
