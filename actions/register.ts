b369922ec0cafacde5fee787d6cbc8c357b058ca
Darkandr1231 <markandrey05@gmail.com>
2025-02-08 16:21:39 +0600

    eleventh

--- actions/register.ts
+++ actions/register.ts
@@ -8,6 +8,7 @@ import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -34,7 +35,10 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     });
 
     const verificationToken = await generateVerificationToken(email);
-    // TODO: Send verification token email
+    await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token,
+    );
 
     return { success: "Confirmation email sent!" };
 };
