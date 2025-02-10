4787b25a0a7c71242c17267e826982ad756b3445
Darkandr1231 <markandrey05@gmail.com>
2025-02-10 15:58:23 +0600

    thirteenth

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,19 +1,39 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
 
+import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper"; 
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () => {
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
+
     const searchParams = useSearchParams();
 
     const token = searchParams.get("token");
 
     const onSubmit = useCallback(() => {
-        console.log(token);
-    }, [token]);
+        if (success || error) return;
+
+        if (!token) {
+            setError("Missing token!");
+            return;
+        }
+
+        newVerification(token)
+            .then((data) => {
+                setSuccess(data.success);
+                setError(data.error);
+            })
+            .catch(() => {
+                setError("Something went wron!");
+            })
+    }, [token, success, error]);
 
     useEffect(() => {
         onSubmit();
@@ -26,7 +46,13 @@ export const NewVerificationForm = () => {
             backButtonHref="/auth/login"
         >
             <div className="flex items-center w-full justify-center">
-                <BeatLoader />
+                {!success && !error && (
+                    <BeatLoader />
+                )}
+                <FormSuccess message={success} />
+                {!success && (
+                    <FormError message={error} />
+                )}
             </div>
         </CardWrapper>
     );
